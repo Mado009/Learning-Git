5bcbd14442acd8a519597b5049d1920d67d9c8fc
Ajni1 <ajnimeta123@gmail.com>
2025-05-31 12:58:08 +0200

    fky

--- src/main/java/org/madprod/productapi/Repository/ProductRepository.java
+++ src/main/java/org/madprod/productapi/Repository/ProductRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     // This interface will automatically provide CRUD operations for Product entities
     // No additional code is needed here as JpaRepository provides all necessary methods
+
 }
