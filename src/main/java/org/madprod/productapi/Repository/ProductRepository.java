8059d55b385f447db36148f01d6e1e74afb9bc3c
Enea <eneazeqo922@gmail.com>
2025-05-31 12:56:02 +0200

    Added List

--- src/main/java/org/madprod/productapi/Repository/ProductRepository.java
+++ src/main/java/org/madprod/productapi/Repository/ProductRepository.java
@@ -3,7 +3,13 @@
 import org.madprod.productapi.Models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
     // This interface will automatically provide CRUD operations for Product entities
     // No additional code is needed here as JpaRepository provides all necessary methods
+
+
+    // Test
+    List<Product> findByNameContainingIgnoreCase(String name);
 }
